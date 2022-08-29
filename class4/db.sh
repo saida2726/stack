5d7999fc9d039f6a64d6d1d611b24f7b27becedd
saida2726 <saidahon007@yahoo.com>
2022-08-29 15:04:38 +0000

    updated env

--- class4/db.sh
+++ class4/db.sh
@@ -2,11 +2,13 @@
 
 
 docker service create   \
----replicas=3   \
----publish mode=host,target=6379,published=6379  \
----name=mysql  \
----constraint=node.role==worker  \
----env MYSQL_ROOT_PASSWORD=my-secret-pw \
+--replicas=3   \
+--publish mode=host,target=6379,published=6379  \
+--name=mysql  \
+--constraint=node.role==worker  \
+--env MYSQL_ROOT_PASSWORD=my-secret-pw \
+--env MYSQL_USER="wordpress"  \
+--env MYSQL_PASSWORD="wordpress" \
 mysql
 
 # bash db.sh
